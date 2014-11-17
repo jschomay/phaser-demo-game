280d1b60e88c3effa3d07a9c90deaae07a159efa
Jeff Schomay <jschomay@gmail.com>
2014-11-17 06:12:29 +0000

    Add basic movement to ship

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var game = new Phaser.Game(800,600, Phaser.AUTO, 'phaser-demo', {preload: preloa
 
 var player;
 var starfield;
+var cursors;
 
 function preload() {
     game.load.image('starfield', 'https://raw.githubusercontent.com/jschomay/phaser-demo-game/master/assets/starfield.png');
@@ -15,12 +16,27 @@ function create() {
     //  The hero!
     player = game.add.sprite(400, 500, 'ship');
     player.anchor.setTo(0.5, 0.5);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    //  And some controls to play the game with
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
     //  Scroll the background
     starfield.tilePosition.y += 2;
 
+    //  Reset the player, then check for movement keys
+    player.body.velocity.setTo(0, 0);
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -200;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 200;
+    }
 }
 
 function render() {
