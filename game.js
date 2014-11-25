fb15eb60e405e35f7425e8527ec73d22b0395be0
Jeff Schomay <jschomay@gmail.com>
2014-11-25 08:17:34 +0000

    Limit firing rate

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ var bank;
 var shipTrail;
 var bullets;
 var fireButton;
+var bulletTimer = 0;
 
 var ACCLERATION = 600;
 var DRAG = 400;
@@ -110,18 +111,25 @@ function render() {
 }
 
 function fireBullet() {
-    var BULLET_SPEED = 400;
-    //  Grab the first bullet we can from the pool
-    var bullet = bullets.getFirstExists(false);
-
-    if (bullet)
+    //  To avoid them being allowed to fire too fast we set a time limit
+    if (game.time.now > bulletTimer)
     {
-        //  And fire it
-        //  Make bullet come out of tip of ship with right angle
-        var bulletOffset = 20 * Math.sin(game.math.degToRad(player.angle));
-        bullet.reset(player.x + bulletOffset, player.y);
-        bullet.angle = player.angle;
-        game.physics.arcade.velocityFromAngle(bullet.angle - 90, BULLET_SPEED, bullet.body.velocity);
-        bullet.body.velocity.x += player.body.velocity.x;
+        var BULLET_SPEED = 400;
+        var BULLET_SPACING = 250;
+        //  Grab the first bullet we can from the pool
+        var bullet = bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+            //  And fire it
+            //  Make bullet come out of tip of ship with right angle
+            var bulletOffset = 20 * Math.sin(game.math.degToRad(player.angle));
+            bullet.reset(player.x + bulletOffset, player.y);
+            bullet.angle = player.angle;
+            game.physics.arcade.velocityFromAngle(bullet.angle - 90, BULLET_SPEED, bullet.body.velocity);
+            bullet.body.velocity.x += player.body.velocity.x;
+
+            bulletTimer = game.time.now + BULLET_SPACING;
+        }
     }
 }
